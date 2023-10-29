08540c8fe1b005a6eeaf42f54a54fe2d4b5ad06a
dongseob <jds3568@naver.com>
2023-10-29 23:50:11 +0900

    후원기능 업데이트

--- src/app/Background.tsx
+++ src/app/Background.tsx
@@ -1,32 +1,32 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
-const Main = () => {
-  const images = ["/background1.jpg", "/background2.jpg", "/background3.jpg"];
+const Background = () => {
+  const images = ["/background1.jpg", "/background2.jpg", "/background3.jpg", "/background4.jpg", "/background5.jpg"];
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const currentImageIndexRef = useRef(currentImageIndex);
+
+  useEffect(() => {
+    currentImageIndexRef.current = currentImageIndex;
+  }, [currentImageIndex]);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      console.log('background')
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 1000);
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % 5);
+    }, 3000);
 
-    return () => {
-      clearInterval(interval);
-    };
+    return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className="absolute inset-0">
-      <picture>
-        <img
-          src={images[currentImageIndex]}
-          alt={`background${currentImageIndex + 1}`}
-          className="w-full h-full object-cover"
-        />
-      </picture>
+    <div className="absolute inset-0 bg-black overflow-hidden">
+      {images.map((imgSrc, index) => (
+        <picture key={index} className="absolute inset-0 transition-opacity duration-1000 ease-in-out" style={{ opacity: index === currentImageIndex ? 1 : 0 }}>
+          <img src={imgSrc} alt={`background${index}`} className="w-full h-full object-cover opacity-30" />
+        </picture>
+      ))}
     </div>
   );
 };
 
-export default Main;
+export default Background;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
-import Image from "next/image";
+"use client";
 import Background from "@/app/Background";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center w-full bg-slate-400">
+    <div className="flex items-center justify-center w-full h-screen">
       <Background></Background>
+      <div className="z-10 text-white">
+        <h1>Want donate</h1>
+        <h2
+          className="ml-8 hover:text-[#ff6347] hover:underline transition-all duration-200"
+          onClick={() => window.open("https://www.buymeacoffee.com/jds3567")}
+        >
+          1. Buy me a coffee
+        </h2>
+
+        <h2 className="ml-8 hover:text-[#ff6347] hover:underline transition-all duration-200" onClick={() => window.open("https://qr.kakaopay.com/FMQhIObcS")}>
+          2. KakaoTalk (only mobile)
+        </h2>
+        <h1>Or say something warm</h1>
+      </div>
     </div>
   );
 }
