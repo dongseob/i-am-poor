29b5a28f330fa12a87ca9d81f9a7446f346a3850
dongseob <jds3568@naver.com>
2023-10-29 23:58:13 +0900

    간격 조정 방명록기능 예정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,22 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center w-full h-screen">
       <Background></Background>
-      <div className="z-10 text-white">
+      <div className="z-10 text-white px-4">
         <h1>Want donate</h1>
         <h2
-          className="ml-8 hover:text-[#ff6347] hover:underline transition-all duration-200"
+          className="ml-4 hover:text-[#ff6347] hover:underline transition-all duration-200"
           onClick={() => window.open("https://www.buymeacoffee.com/jds3567")}
         >
           1. Buy me a coffee
         </h2>
-
-        <h2 className="ml-8 hover:text-[#ff6347] hover:underline transition-all duration-200" onClick={() => window.open("https://qr.kakaopay.com/FMQhIObcS")}>
+        <h2 className="ml-4 hover:text-[#ff6347] hover:underline transition-all duration-200" onClick={() => window.open("https://qr.kakaopay.com/FMQhIObcS")}>
           2. KakaoTalk (only mobile)
         </h2>
+
+        <div className="h-2"></div>
+
         <h1>Or say something warm</h1>
+        <input type="text" name="" id="" />
       </div>
     </div>
   );
